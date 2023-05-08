0a57c94e55456b426a27dbd830f0d6d990d51371
MAlabdulhadi <65816656+MAlabdulhadi@users.noreply.github.com>
2023-05-08 14:13:17 +0300

    Add files via upload

--- homework7/src/Main.java
+++ homework7/src/Main.java
@@ -3,24 +3,38 @@ public class Main {
     public static void main(String[] args) {
 
         // test class 1
+
         Account a1 = new Account("444","mohammed");
+        System.out.println("info about account "+a1.getName());
         System.out.println(a1.toString());
         a1.debit(100);
+        System.out.println("info after debit");
         System.out.println(a1.toString());
         a1.credit(50);
+        System.out.println("info after credit");
         System.out.println(a1.toString());
+
         Account a2 = new Account("555","Ali");
+        System.out.println("info about account "+a2.getName());
         System.out.println(a2.toString());
+        System.out.println("transfer form "+a1.getName()+" to "+a2.getName());
         a1.transferTo(a2,20);
+        System.out.println("info about account "+a2.getName()+" after transfer");
         System.out.println(a2.toString());
+        System.out.println("info about account "+a1.getName()+" after transfer");
         System.out.println(a1.toString());
 
         // test class 2
+
         System.out.println("______________");
+
         Employee e1 = new Employee("4","saad",5000);
+        System.out.println("info employee "+e1.getName());
         System.out.println(e1.toString());
+        System.out.println("AnnualSalary : ");
         System.out.println(e1.getAnnualSalary());
         e1.raisedSalary(10);
+        System.out.println("info employee after raised Salary : by 10% ");
         System.out.println(e1.toString());
 
 
